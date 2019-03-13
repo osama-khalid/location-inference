77010cb6c6d2c0de022a165bb41189e752c8b32e
osama-khalid <khalyd.osama@gmail.com>
2019-03-12 19:43:28 -0700

    Add files via upload

--- plot.py
+++ plot.py
@@ -1,18 +1,44 @@
+import numpy as np
 file=open('mwFreq.txt','r').read().split('\n')
 Y=[]
+
 X=[]
+
+A={}
+B={}
 for f in file:
     if len(f)>0:
         row=f.split('|')
-        X.append(float(row[1])*100)
-        Y.append(float(row[2])*100)
+        A[row[0]]=float(row[1])
+        B[row[0]]=np.round(float(row[2]),5)
+
+B2={}        
+A2={}
+for b in B:
+    if B[b] not in B2:
+        B2[B[b]]=0
+    B2[B[b]]=B2[B[b]]+A[b]
+
         
-import numpy as np
+import operator
+
+sortbase=sorted(B.items(),key=operator.itemgetter(1),reverse=True)[::-1]
+p=[]
+for s in sortbase:
+    p.append(s[1])
+p=list(set(p))
+p.sort()
+for s in p:    
+    Y.append(B2[s]*float(100))
+    X.append(s)
+        
+
 from matplotlib import pyplot
 from numpy import cumsum
-X2=np.arange(0,len(file)-1)
+#X2=np.arange(0,len(X))
+#X2=X2/float(len(X))
 CY=cumsum(Y)
-pyplot.plot(X2,CY)
-pyplot.xlabel('Number of Unique MaxWords', fontsize=16)
-pyplot.ylabel('Percentage of Background Words Covered', fontsize=16)
+pyplot.plot(X,CY)
+pyplot.xlabel('Proportion of Background Words', fontsize=16)
+pyplot.ylabel('Percentage of Maxwords', fontsize=16)
 pyplot.show()
